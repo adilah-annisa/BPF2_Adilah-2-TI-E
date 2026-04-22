2b82437a51a6638874d72075756a14198b5cc5e1
adilah-annisa <adilah24ti@mahasiswa.pcr.ac.id>
2026-04-22 09:24:21 +0700

    Pertemuan 6
    - Modifikasi Font, Warna
    - Penggunaan Template Website Modul

--- react-adilah-2tie/adilah-app/src/pertemuan-5/FrameworkList.jsx
+++ react-adilah-2tie/adilah-app/src/pertemuan-5/FrameworkList.jsx
@@ -1,8 +1,10 @@
+import { AiTwotoneHome } from "react-icons/ai"; 
 import frameworkData from "./framework.json";
 
 export default function FrameworkList() {
     return (
         <div className="p-8">
+            <AiTwotoneHome/>
             {frameworkData.map((item) => (
 		            <div key={item.id} className="border p-4 mb-4 rounded-lg shadow-md bg-white">
 		                <h2 className="text-lg font-bold text-gray-800">{item.name}</h2>
---
+++ react-adilah-2tie/adilah-app/src/pertemuan-6/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from "react-dom/client";
+import "./assets/tailwind.css";
+import Sidebar from "./layouts/Sidebar";
+import Header from "./layouts/Header";
+import Dashboard from "./pages/Dashboard";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <div id="app-container" className="bg-gray-100 min-h-screen flex">
+      <div id="layout-wrapper" className="flex flex-row flex-1">
+        <Sidebar />
+        <div id="main-content" className="flex-1 p-4">
+          <Header />
+          <Dashboard />
+        </div>
+      </div>
+    </div>
+  </div>,
+);
